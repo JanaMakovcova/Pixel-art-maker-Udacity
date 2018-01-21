1fd5ca644a8459d3435d2ed047e4954547196690
Jana Makovcova <jana.makovcova@centrum.cz>
2018-01-21 22:45:06 +0100

    prvni pokus, ovsem zatim nefunkcni

--- designs.js
+++ designs.js
@@ -1,10 +1,25 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+var barva =$('#colorPicker').val();
+var height = $('#input_height').val();
+var width = $('#input_width').val();
+var tabulka = $('#pixel_canvas');
 
-function makeGrid() {
 
+function makeGrid(i, j) {
+  var retezec  = '';
+for (var i = 0; i < height; i++) {
+  retezec = retezec +'<tr>';
+  for (var j = 0; j < width; j++) {
+    retezec = retezec +'<td></td>';
+  }
+  retezec = retezec + '</tr>';
+}
+return retezec;
 // Your code goes here!
+}
 
+tabulka.html(makeGrid(height,width));
+function setSize(){
+  tabulka.html(makeGrid(height,width));
+  //$('#input_height').attr("value", height.toString());
 }
